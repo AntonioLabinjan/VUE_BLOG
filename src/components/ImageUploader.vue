386cbed343c90ededed980d7b20b96a7083b74ec
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-09-27 12:59:38 +0200

    Update ImageUploader.vue

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <center><h2>VueBlog</h2></center>
+    <center><h1>VueBlog</h1></center>
     <input type="file" @change="uploadImages" multiple>
-    <textarea v-model="postText" placeholder="Unesite tekstualni post"></textarea>
-    <button @click="addPost">Dodaj post</button>
+    <textarea v-model="postText" placeholder="Unesite tekst objave"></textarea>
+    <input v-model="videoLink" placeholder="Unesite URL videa">
+    <button @click="addPost">Dodaj objavu</button>
+    <button @click="addVideo">Dodaj video</button>
     <div v-for="(item, index) in items" :key="index">
       <div v-if="item.type === 'image'">
         <img :src="item.url" alt="Prenesena slika">
@@ -19,12 +21,34 @@
           <button @click="deleteDescription(index)">Obriši opis</button>
         </div>
         <button @click="deleteImage(index)">Obriši sliku</button>
+        <!-- Dodaj ikonu srca za lajk -->
+        <span @click="toggleLike(index)">
+          <i class="fas fa-heart" :class="{ 'liked': item.liked }"></i>
+        </span>
       </div>
-      <div v-else>
+      <div v-else-if="item.type === 'text'">
         <p>Datum i vrijeme postavljanja: {{ item.date }}</p>
         <p>{{ item.text }}</p>
-        <button @click="editText(index)">Uredi tekst</button>
-        <button @click="deleteText(index)">Obriši tekst</button>
+        <button @click="editText(index)">Uredi objavu</button>
+        <button @click="deleteText(index)">Obriši objavu</button>
+        <!-- Dodaj ikonu srca za lajk -->
+        <span @click="toggleLike(index)">
+          <i class="fas fa-heart" :class="{ 'liked': item.liked }"></i>
+        </span>
+      </div>
+      <div v-else-if="item.type === 'video'">
+        <iframe
+          width="560"
+          height="315"
+          :src="getEmbedUrl(item.url)"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+        <p>Datum i vrijeme postavljanja: {{ item.date }}</p>
+         <button @click="deleteVideo(index)">Obriši video</button>
+        <span @click="toggleLike(index)">
+          <i class="fas fa-heart" :class="{ 'liked': item.liked }"></i>
+        </span>
       </div>
     </div>
   </div>
@@ -34,8 +58,9 @@
 export default {
   data() {
     return {
-      items: [], // Pohranjuje slike, tekstualne postove i njihove informacije
-      postText: "", // Tekstualni post
+      items: [], 
+      postText: "", 
+      videoLink: "",
     };
   },
   methods: {
@@ -46,13 +71,14 @@ export default {
         if (file) {
           const reader = new FileReader();
           reader.onload = () => {
-            const date = new Date(); // trenutno vrijeme
+            const date = new Date(); 
             this.items.push({
               type: 'image',
               url: reader.result,
               date: date.toLocaleString(),
-              description: "", // Početni prazni opis slike
-              editDescription: "" // Polje za uređivanje opisa
+              description: "", 
+              editDescription: "", 
+              liked: false 
             });
           };
           reader.readAsDataURL(file);
@@ -61,41 +87,154 @@ export default {
     },
     addPost() {
       if (this.postText.trim() !== "") {
-        const date = new Date(); // trenutno vrijeme
+        const date = new Date(); 
         this.items.push({
           type: 'text',
           date: date.toLocaleString(),
-          text: this.postText
+          text: this.postText,
+          liked: false 
         });
-        this.postText = ""; // Resetiranje unosa teksta
+        this.postText = ""; 
       }
     },
     deleteImage(index) {
       this.items.splice(index, 1);
     },
     saveDescription(index) {
-      // Spremi opis slike
       this.items[index].description = this.items[index].editDescription.trim();
-      this.items[index].editDescription = ""; // Resetiraj uređivanje opisa
+      this.items[index].editDescription = ""; 
     },
     deleteDescription(index) {
-      // Obriši opis slike
       this.items[index].description = "";
-      this.items[index].editDescription = ""; // Resetiraj uređivanje opisa
+      this.items[index].editDescription = "";
     },
     editText(index) {
-      // Omogućava uređivanje teksta posta
       const editedText = prompt("Uredi tekst posta", this.items[index].text);
       if (editedText !== null) {
         this.items[index].text = editedText.trim();
       }
     },
     deleteText(index) {
-      // Omogućava brisanje teksta posta
       if (confirm("Jeste li sigurni da želite obrisati ovaj tekst?")) {
         this.items.splice(index, 1);
       }
-    }
-  }
+    },
+    addVideo() {
+      if (this.videoLink.trim() !== "") {
+        const date = new Date(); // trenutno vrijeme
+        this.items.push({
+          type: 'video',
+          url: this.videoLink,
+          date: date.toLocaleString(),
+          liked: false
+        });
+        this.videoLink = "";
+      }
+    },
+    toggleLike(index) {
+      const post = this.items[index];
+      if (post.liked) {
+        // Post je već lajkan, trebamo ga "unlajkati"
+        post.liked = false;
+      } else {
+        // Post nije lajkan, lajkajmo ga
+        post.liked = true;
+      }
+    },
+    getEmbedUrl(url) {
+      if (url.includes('youtube.com')) {
+        const videoId = url.split('v=')[1];
+        return `https://www.youtube.com/embed/${videoId}`;
+      }
+      return url;
+    },
+    deleteVideo(index) {
+      if(confirm('Jeste li sigurni da želite obrisati video?')){
+        this.items.splice(index,1);
+      }
+    },
+  },
 };
 </script>
+
+<style scoped>
+/* Stil za lajk ikonu */
+.liked {
+  color: red; /* Promijenite boju ikone kada je lajkano */
+  cursor: pointer; /* Dodajte pokazivač ruke za lajk ikonu */
+}
+/* Resetiranje osnovnih stilova */
+body, html {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+}
+
+/* Postavljanje pozadinske boje za cijelu stranicu */
+body {
+  background-color: #f2f2f2;
+}
+
+/* Glavni naslov */
+h2 {
+  text-align: center;
+  color: #333;
+  padding: 20px;
+}
+
+/* Unos teksta i gumb za dodavanje posta */
+input[type="file"], textarea {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+button {
+  display: block;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 10px 0;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 11px;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+/* Stil za pojedinačni post */
+div {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin: 10px 0;
+  padding: 20px;
+  position: relative;
+}
+
+/* Stil za sliku u postu */
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+/* Stil za datum i vrijeme */
+p {
+  color: #777;
+  font-size: 14px;
+  margin-top: 10px;
+}
+
+/* Stil za uređivanje i brisanje teksta/posta */
+button:hover {
+  background-color: #ff6347;
+  color: #fff;
+}
+</style>
