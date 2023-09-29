39f7df34c9af1c965ac027c641df265ed25bbe1e
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-09-29 11:11:47 +0200

    Update main.js
    Added CSS for social media links

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+  integrity="sha384-6v6J5F5O6bk9a16ag7dlCGy8Fb7pUwq5Ae4G8t5z4K4b9fpn5O5fe5O5fe5O5fe5O5fe5O5fe5O5fe5O5fe5O5fe5O5fe5O5fe5"
+  crossorigin="anonymous"
+/>
 import { createApp } from 'vue'
 import App from './App.vue'
 
