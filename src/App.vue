492d9b708a4cea491b3186e668006b4cd6781e9e
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-09-29 11:11:14 +0200

    Update App.vue
    Added social media links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
-    <ImageUploader />
-    <ImageGallery />   
+    <ImageUploader @filter="handleFilter" />
+    <ImageGallery :items="filteredItems" />
+	<center><h5>Also visit:</h5></center>
+    <div class="social-icons">
+      <a href="https://www.facebook.com" target="_blank">
+        <i class="fab fa-facebook"></i> <!-- Font Awesome Facebook ikonica -->
+      </a>
+      <a href="https://www.youtube.com" target="_blank">
+        <i class="fab fa-youtube"></i> <!-- Font Awesome YouTube ikonica -->
+      </a>
+      <a href="https://www.twitter.com" target="_blank">
+        <i class="fab fa-twitter"></i> <!-- Font Awesome Twitter ikonica -->
+      </a>
+      <a href="https://www.instagram.com" target="_blank">
+        <i class="fab fa-instagram"></i> <!-- Font Awesome Instagram ikonica -->
+      </a>
+      <a href="https://www.discord.com" target="_blank">
+        <i class="fab fa-discord"></i> <!-- Font Awesome Discord ikonica -->
+      </a>
+    </div>
   </div>
 </template>
+
 
 <script>
 import ImageUploader from "./components/ImageUploader.vue";
@@ -14,6 +33,51 @@ export default {
   components: {
     ImageUploader,
     ImageGallery,
-  }
+  },
+  data() {
+    return {
+      items: [],
+      searchQuery: "",
+    };
+  },
+  computed: {
+    filteredItems() {
+      const query = this.searchQuery.toLowerCase();
+      return this.items.filter(item => {
+        if (item.type === 'text') {
+          return item.text.toLowerCase().includes(query);
+        } else if (item.type === 'image') {
+          return (
+            item.description.toLowerCase().includes(query) ||
+            item.date.toLowerCase().includes(query)
+          );
+        }
+        return false;
+      });
+    },
+  },
+  methods: {
+    handleFilter(query) {
+      this.searchQuery = query;
+    },
+  },
 }
 </script>
+
+<style scoped>
+.social-icons {
+  margin-top: 20px;
+  text-align: center; 
+}
+
+.social-icons a {
+  margin-right: 10px; 
+  display: inline-block;
+}
+
+.social-icons i {
+  font-size: 36px; 
+  color: #007bff; 
+}
+</style>
+
