f21673ddfbe54de336615ca6d1b2f8cbe6391d53
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2023-10-03 11:18:39 +0200

    Update ImageGallery.vue

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,18 +1,63 @@
 <template>
   <div>
+    <center><h1>VueBlog</h1></center>
+    <input type="file" @change="uploadImages" multiple>
+    <textarea v-model="postText" placeholder="Type in post text"></textarea>
+    <input v-model="videoLink" placeholder="Type in video URL">
+    <button @click="addPost">Add post</button>
+    <button @click="addVideo">Add video</button>
     <div v-for="(item, index) in items" :key="index">
-      <img v-if="item.type === 'image'" :src="item.url" alt="Slika">
-      <iframe v-else :src="item.url" frameborder="0" allowfullscreen></iframe>
-      <p>Uploaded on: {{ item.date }}</p>
-      <div>
+      <div v-if="item.type === 'image'">
+        <img :src="item.url" alt="Uploaded image">
+        <p>Uploaded on: {{ item.date }}</p>
+        <div>
+          <p v-if="item.description">{{ item.description }}</p>
+          <input
+            type="text"
+            v-model="item.editDescription"
+            placeholder="Type in or edit picture description"
+          >
+          <button @click="saveDescription(index)">Save description</button>
+          <button @click="deleteDescription(index)" class="delete">Delete description</button>
+        </div>
+        <button @click="deleteImage(index)" class="delete">Delete image</button>
         <span @click="toggleLike(index)">
-          <i class="fas fa-heart" :class="{ 'liked': item.liked, 'disliked': item.disliked }"></i>
+          <i class="fas fa-heart" :class="{ 'liked': item.liked }"></i>
         </span>
         <span @click="toggleDislike(index)">
-          <i class="fas fa-thumbs-down" :class="{ 'disliked': item.disliked, 'liked': item.liked }"></i>
+          <i class="fas fa-thumbs-down" :class="{ 'disliked': item.disliked }"></i>
+        </span>
+      </div>
+      <div v-else-if="item.type === 'text'">
+        <p>Posted on: {{ item.date }}</p>
+        <p>{{ item.text }}</p>
+        <button @click="editText(index)">Edit post</button>
+        <button @click="deleteText(index)" class="delete">Delete post</button>
+        <span @click="toggleLike(index)">
+          <i class="fas fa-heart" :class="{ 'liked': item.liked }"></i>
+        </span>
+        <span @click="toggleDislike(index)">
+          <i class="fas fa-thumbs-down" :class="{ 'disliked': item.disliked }"></i>
         </span>
         <span @click="copyText(item.text)">
-          <i class="fas fa-clipboard"></i> Copy
+          <i class="fas fa-copy"></i> Copy
+        </span>
+      </div>
+      <div v-else-if="item.type === 'video'">
+        <iframe
+          width="560"
+          height="315"
+          :src="getEmbedUrl(item.url)"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+        <p>Posted on: {{ item.date }}</p>
+         <button @click="deleteVideo(index)" class="delete">Delete video</button>
+        <span @click="toggleLike(index)">
+          <i class="fas fa-heart" :class="{ 'liked': item.liked }"></i>
+        </span>
+        <span @click="toggleDislike(index)">
+          <i class="fas fa-thumbs-down" :class="{ 'disliked': item.disliked }"></i>
         </span>
       </div>
     </div>
@@ -20,22 +65,92 @@
 </template>
 
 <script>
-import '@fortawesome/fontawesome-free/css/all.css';
 export default {
-  props: {
-    items: Array,
-	searchQuery: String,
+  data() {
+    return {
+      items: [], 
+      postText: "", 
+      videoLink: "",
+    };
   },
   methods: {
+    uploadImages(event) {
+      const files = event.target.files;
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = () => {
+            const date = new Date(); 
+            this.items.push({
+              type: 'image',
+              url: reader.result,
+              date: date.toLocaleString(),
+              description: "", 
+              editDescription: "", 
+              liked: false,
+              disliked: false
+            });
+          };
+          reader.readAsDataURL(file);
+        }
+      }
+    },
+    addPost() {
+      if (this.postText.trim() !== "") {
+        const date = new Date(); 
+        this.items.push({
+          type: 'text',
+          date: date.toLocaleString(),
+          text: this.postText,
+          liked: false,
+          disliked: false
+        });
+        this.postText = ""; 
+      }
+    },
+    deleteImage(index) {
+      this.items.splice(index, 1);
+    },
+    saveDescription(index) {
+      this.items[index].description = this.items[index].editDescription.trim();
+      this.items[index].editDescription = ""; 
+    },
+    deleteDescription(index) {
+      this.items[index].description = "";
+      this.items[index].editDescription = "";
+    },
+    editText(index) {
+      const editedText = prompt("Uredi tekst posta", this.items[index].text);
+      if (editedText !== null) {
+        this.items[index].text = editedText.trim();
+      }
+    },
+    deleteText(index) {
+      if (confirm("Are you sure you want to delete?")) {
+        this.items.splice(index, 1);
+      }
+    },
+    addVideo() {
+      if (this.videoLink.trim() !== "") {
+        const date = new Date(); 
+        this.items.push({
+          type: 'video',
+          url: this.videoLink,
+          date: date.toLocaleString(),
+          liked: false,
+          disliked: false
+        });
+        this.videoLink = "";
+      }
+    },
     toggleLike(index) {
       const post = this.items[index];
       if (post.liked) {
         post.liked = false;
       } else {
         post.liked = true;
-        if (post.disliked) {
-          post.disliked = false;
-        }
+        post.disliked = false;
       }
     },
     toggleDislike(index) {
@@ -44,17 +159,27 @@ export default {
         post.disliked = false;
       } else {
         post.disliked = true;
-        if (post.liked) {
-          post.liked = false;
-        }
+        post.liked = false;
       }
     },
-	copyText(text) {
+    getEmbedUrl(url) {
+      if (url.includes('youtube.com')) {
+        const videoId = url.split('v=')[1];
+        return `https://www.youtube.com/embed/${videoId}`;
+      }
+      return url;
+    },
+    deleteVideo(index) {
+      if(confirm('Are you sure you want to delete this video?')){
+        this.items.splice(index,1);
+      }
+    },
+    copyText(text) {
       const textarea = document.createElement("textarea");
       textarea.value = text;
       document.body.appendChild(textarea);
       textarea.select();
-      textarea.setSelectionRange(0, 99999); 
+      textarea.setSelectionRange(0, 99999);
       document.execCommand("copy");
       document.body.removeChild(textarea);
       alert("Text copied to clipboard.");
@@ -68,12 +193,10 @@ export default {
   color: red; 
   cursor: pointer; 
 }
-
 .disliked {
-	color: red;
-	cursor: pointer;
+  color: red;
+  cursor: pointer;
 }
-
 
 body, html {
   margin: 0;
@@ -83,7 +206,7 @@ body, html {
 
 
 body {
-  background-color: light-blue;
+  background-color: lightblue;
 }
 
 
@@ -101,7 +224,7 @@ input[type="file"], textarea {
   margin: 10px 0;
   border: 1px solid #ccc;
   border-radius: 5px;
-  font-size: 11px;
+  font-size: 16px;
 }
 
 button {
@@ -113,9 +236,13 @@ button {
   margin: 10px 0;
   border-radius: 5px;
   cursor: pointer;
-  font-size: 16px;
+  font-size: 11px;
 }
 
+button.delete {
+ background-color: #ff6347;
+ color: #fff;
+}
 button:hover {
   background-color: #0056b3;
 }
@@ -130,16 +257,10 @@ div {
   position: relative;
 }
 
-
 img {
   max-width: 100%;
   height: auto;
-  border: 1px solid #ccc; 
-  border-radius: 5px; 
-  padding: 5px; 
-  margin-bottom: 10px;
 }
-
 
 
 p {
@@ -154,4 +275,7 @@ button:hover {
   color: #fff;
 }
 
+h1 {
+  color: #007bff;
+}
 </style>
